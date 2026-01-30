e6c8bb9ea163ce34ed64de39714dfa22f164c00e
Mrittick Deb <debmrittick@gmail.com>
2026-01-30 19:35:00 +0530

    commit

---
+++ day8/index.js
@@ -0,0 +1,63 @@
+
+// let a=20;//globalscope
+// function value() {
+//     console.log(a)
+// }
+
+// value()
+
+/*
+function value(){ //functional scope
+    let a=20;
+    console.log(a)
+}
+value()
+console.log(a)
+*/
+
+
+// for(let i=0;i<5;i++){
+//     console.log(i);
+// }
+// console.log(i);
+
+
+
+
+
+
+
+
+//console.log(a);
+// function pr(){
+//     var a=20;
+//     console.log(a);
+// }
+// pr()
+// console.log(a)
+
+
+
+
+
+
+
+// var a="hello"
+// if(true){
+//     let b=20;
+//     console.log(a);
+// }
+// console.log(b)
+
+
+// for(let i=0;i<5;i++){
+//     setTimeout(()=>{
+//         console.log(i)
+//     },i*1000)
+// }
+
+
+
+
+
+
